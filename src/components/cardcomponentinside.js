8c166415efefe2f3c068fdad1de68bec7f1843dc
saroopjack <saroop.jack@gmail.com>
2021-06-05 16:43:01 +0530

    added link

--- src/components/cardcomponentinside.js
+++ src/components/cardcomponentinside.js
@@ -6,11 +6,14 @@ export default function Cardcomponentinside(props) {
     <div>
       <Card style={{ width: "16rem" }}>
         <Card.Img variant="top" src={props.img} style={{ height: "200px" }} />
-        <Card.Body
-          style={{ backgroundColor: "teal", color: "white" }}
-        >
-          <Card.Title style={{textAlign:"center"}}>{props.title}</Card.Title>
+        <Card.Body style={{ backgroundColor: "teal", color: "white" }}>
+          <Card.Title style={{ textAlign: "center" }}>{props.title}</Card.Title>
           <Card.Text>{props.details}</Card.Text>
+          <div style={{ textAlign: "center"}} >
+            <a style={{ color:"white"}} href={props.ytLink}>
+              YouTube Link
+            </a>
+          </div>
         </Card.Body>
       </Card>
     </div>
--- src/malyalamMovies.js
+++ src/malyalamMovies.js
@@ -4,26 +4,79 @@ import premam from "./assets/premam.jpg";
 import Cardcomponentinside from "./components/cardcomponentinside";
 
 export default function malyalamMovies(props) {
+  const movieList = [
+    {
+      name: "Premam",
+      coverPhoto: premam,
+      desc: "Blockbustar malayalam movie",
+      youtubeLink: "https://www.youtube.com/watch?v=yjaFvFuQ-QM"
+    },
+    {
+      name: "God Father",
+      coverPhoto: "godfather",
+      desc: "Blockbustar malayalam movie 90",
+      youtubeLink:"https://www.youtube.com/watch?v=hsRMPw5HkWo"
+    },
+    {
+      name: "Devasuram",
+      coverPhoto: "premam",
+      desc: "Blockbustar malayalam movie",
+      youtubeLink:"https://www.youtube.com/watch?v=hsRMPw5HkWo"
+
+    },
+    {
+      name: "Big B",
+      coverPhoto: "premam",
+      desc: "Blockbustar malayalam movie",
+      youtubeLink:""
+    },
+    {
+      name: "T20- T20",
+      coverPhoto: "premam",
+      desc: "Blockbustar malayalam movie",
+      youtubeLink:""
+    },
+    {
+      name: "T20- T20",
+      coverPhoto: "premam",
+      desc: "Blockbustar malayalam movie",
+      youtubeLink:""
+    },
+    {
+      name: "T20- T20",
+      coverPhoto: "premam",
+      desc: "Blockbustar malayalam movie",
+      youtubeLink:""
+    },
+  ];
   const backToHome = () => {
     props.history.push("/");
   };
   return (
-    <div>
+    <div className="container">
       <div className="headingBar">
         <h1 style={{ backgroundColor: "crimson", padding: "10px" }}>
           MalyalamMovies
         </h1>
-        <Button style={{ backgroundColor: "purple"}} onClick={backToHome} >
+        <Button style={{ backgroundColor: "purple" }} onClick={backToHome}>
           Back To Home
         </Button>
       </div>
       <br />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Cardcomponentinside
-          img={premam}
-          title="Premam"
-          details="Blockbustar malayalam movie"
-        />
+      <div className="row">
+        {movieList.map((movie) => {
+          return (
+            <div className="col-3" style={{ padding: "1%" }}>
+              <Cardcomponentinside
+                img={movie.coverPhoto}
+                title={movie.name}
+                details={movie.desc}
+                ytLink={movie.youtubeLink}
+
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
